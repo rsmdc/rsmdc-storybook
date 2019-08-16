6b79a5e2ce3fe65da2bea191ec085ffa1eaf5c82
kokami <mooooonaka@gmail.com>
2019-08-16 12:33:56 +0900

    feat: Add switch and slider

---
+++ stories/slider.stories.js
@@ -0,0 +1,84 @@
+import { storiesOf } from '@storybook/html'
+import { Slider } from '@rsmdc/rsmdc'
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs'
+import { withLinks } from '@storybook/addon-links'
+import copyCodeBlock from '@pickra/copy-code-block'
+
+import './css/style.scss'
+import './css/slider.scss'
+import 'highlight.js'
+import 'highlight.js/styles/a11y-light.css'
+
+storiesOf('Components|Slider', module)
+  .addDecorator(withLinks)
+  .add('概要', () => `
+    <h4>Slider</h4>
+    <p>スライダーを使用すると、ユーザーは値の範囲から選択できる。</p>
+    参考: <a href="https://material.io/design/components/sliders.html#">https://material.io/design/components/sliders.html#</a>
+
+  `)
+  .add('使用方法', () => `
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-slider class="slider" />`,
+      { lang: 'html' }
+    )}
+    <h4>HTML要素</h4>
+    <p>custom elementsを<code>rs</code>のプレフィックスをつけて定義した前提です。<br>
+      下記HTML要素を使うことで、custom elementsが表示されます。
+    </p>
+    <table>
+      <tr>
+        <th>要素名</th>
+        <th>説明</th>
+        <th>許可されている子要素</th>
+        <th>備考</th>
+      </tr>
+      <tr>
+        <td>rs-slider</td>
+        <td>スライダー本体を表示します</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+    </table>
+    <h4>HTML属性</h4>
+    <p>下記HTML属性をつけることで、custom elementsの状態が変化します</p>
+    <table>
+      <tr>
+        <th>属性</th>
+        <th>説明</th>
+        <th>タイプ</th>
+        <th>備考</th>
+      </tr>
+      <tr>
+        <td>disabled</td>
+        <td>スライダーを無効にします</td>
+        <td>Boolean</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>min</td>
+        <td>最小値を設定します</td>
+        <td>number</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>max</td>
+        <td>最大値を設定します</td>
+        <td>number</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>countable</td>
+        <td>カウンターを表示します</td>
+        <td>Boolean</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>marked</td>
+        <td>マーカーを表示します</td>
+        <td>Boolean</td>
+        <td>-</td>
+      </tr>
+    </table>
+  `)
---
+++ stories/switch.stories.js
@@ -0,0 +1,79 @@
+import { storiesOf } from '@storybook/html'
+import { Switch } from '@rsmdc/rsmdc'
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs'
+import { withLinks } from '@storybook/addon-links'
+import copyCodeBlock from '@pickra/copy-code-block'
+
+import './css/style.scss'
+import './css/switch.scss'
+import 'highlight.js'
+import 'highlight.js/styles/a11y-light.css'
+
+
+storiesOf('Components|Switch', module)
+  .addDecorator(withLinks)
+  .add('概要', () => `
+    <h4>Switch</h4>
+    <p>スイッチは単一の設定の状態をオンまたはオフに切り替える。</p>
+    参考: <a href="https://material.io/design/components/selection-controls.html#switches">https://material.io/design/components/selection-controls.html#switches</a>
+
+  `)
+  .add('使用方法', () => `
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-switch class="switch" />`,
+      { lang: 'html' }
+    )}
+    <h4>HTML要素</h4>
+    <p>custom elementsを<code>rs</code>のプレフィックスをつけて定義した前提です。<br>
+      下記HTML要素を使うことで、custom elementsが表示されます。
+    </p>
+    <table>
+      <tr>
+        <th>要素名</th>
+        <th>説明</th>
+        <th>許可されている子要素</th>
+        <th>備考</th>
+      </tr>
+      <tr>
+        <td>rs-switch</td>
+        <td>スイッチ本体を表示します</td>
+        <td>-</td>
+        <td>-</td>
+      </tr>
+    </table>
+    <h4>HTML属性</h4>
+    <p>下記HTML属性をつけることで、custom elementsの状態が変化します</p>
+    <table>
+      <tr>
+        <th>属性</th>
+        <th>説明</th>
+        <th>タイプ</th>
+        <th>備考</th>
+      </tr>
+      <tr>
+        <td>id</td>
+        <td>スイッチとラベルを紐付けます</td>
+        <td>String</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>label</td>
+        <td>ラベルを表示します</td>
+        <td>String</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>checked</td>
+        <td>スイッチを選択された状態にします</td>
+        <td>Boolean</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>disabled</td>
+        <td>スイッチを無効にします</td>
+        <td>Boolean</td>
+        <td>-</td>
+      </tr>
+    </table>
+  `)
