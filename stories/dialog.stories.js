7029ab80cea9ade579915daefa2d6b9c42b5afc2
kokami <mooooonaka@gmail.com>
2019-08-29 10:31:18 +0900

    feat: Add dialog

--- stories/dialog.stories.js
+++ stories/dialog.stories.js
@@ -9,28 +9,30 @@ import './css/slider.scss'
 import 'highlight.js'
 import 'highlight.js/styles/a11y-light.css'
 
-storiesOf('Components|Snackbar', module)
+storiesOf('Components|Dialog', module)
   .addDecorator(withLinks)
   .add('概要', () => `
-    <h4>Snackbar</h4>
-    <p>アプリプロセスに関する簡単なメッセージを表示する</p>
-    参考: <a href="https://material.io/design/components/snackbars.html">https://material.io/design/components/snackbars.html</a>
-    <p>スナックバーのタイプ</p>
+    <h4>Dialog</h4>
+    <p>ダイアログは、ユーザーによる意思決定や追加情報の入力用などに表示される。</p>
+    参考: <a href="https://material.io/components/dialogs/#">https://material.io/components/dialogs/#</a>
+
+    <p>ダイアログのタイプ</p>
     <ul>
       <li>normal（デフォルト）</li>
-      <li>leading：スナックバー本体を左端に表示</li>
-      <li>stacked：テキストとアクション部分を改行して表示</li>
+      <li>stacked：ボタンが改行された状態で表示</li>
     </ul>
+
   `)
   .add('使用方法', () => `
     <h4>使用方法</h4>
     ${copyCodeBlock(
-`<rs-snackbar class="snackbar">
-  <rs-snackbar-text>sample</rs-snackbar-text>
-</rs-snackbar>
+`<rs-dialog class="dialog">
+  <rs-dialog-title>title</rs-dialog-title>
+  <rs-dialog-content>sample</rs-dialog-content>
+</rs-dialog>
 `,
-      { lang: 'html' }
-    )}
+    { lang: 'html' }
+  )}
     <h4>HTML要素</h4>
     <p>custom elementsを<code>rs</code>のプレフィックスをつけて定義した前提です。<br>
       下記HTML要素を使うことで、custom elementsが表示されます。
@@ -43,20 +45,19 @@ storiesOf('Components|Snackbar', module)
         <th>備考</th>
       </tr>
       <tr>
-        <td>rs-snackbar</td>
-        <td>スナックバー本体を表示します</td>
-        <td>rs-snackbar-text</td>
-        <td>-</td>
-      </tr>
-      <tr>
-        <td>rs-snackbar-text</td>
-        <td>テキスト部分を表示します</td>
-        <td>-</td>
+        <td>rs-dialog</td>
+        <td>ダイアログ本体を表示します</td>
+        <td>
+          <ul>
+            <li>rs-dialog-title</li>
+            <li>rs-dialog-content</li>
+          </ul>
+        </td>
         <td>-</td>
       </tr>
     </table>
     <h4>HTML属性</h4>
-    <p>下記HTML属性をつけることで、custom elementsの状態が変化します。</p>
+    <p>下記HTML属性をつけることで、custom elementsの状態が変化します</p>
     <table>
       <tr>
         <th>属性</th>
@@ -65,16 +66,17 @@ storiesOf('Components|Snackbar', module)
         <th>備考</th>
       </tr>
       <tr>
-        <td>opened</td>
-        <td>スナックバーを表示状態にします。</td>
+        <td>opend</td>
+        <td>ダイアログを表示状態にします。</td>
         <td>Boolean</td>
         <td>-</td>
       </tr>
     </table>
+
     <h4>テーマ変数</h4>
     <p>
-      rs-snackbarで使われるテーマ変数です。 <br>
-      下記変数に新しく色を指定することで、テーマカラーが上書きされます。
+    　rs-dialogで使われるテーマ変数です。 <br>
+    　下記変数に新しく色を指定することで、テーマカラーが上書きされます。
     </p>
     <table>
       <tr>
@@ -82,8 +84,12 @@ storiesOf('Components|Snackbar', module)
         <th>説明</th>
       </tr>
       <tr>
+        <td>$rs-theme-surface</td>
+        <td>選択状態の背景色に関わります。</td>
+      </tr>
+      <tr>
         <td>$rs-theme-on-surface</td>
-        <td>背景色に関わります。</td>
+        <td>選択状態のテキストの色に関わります。</td>
       </tr>
     </table>
   `)
