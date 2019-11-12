43ded15ec44fdb542386a0270aa6801bd2a42bfb
kokami <mooooonaka@gmail.com>
2019-11-12 11:58:58 +0900

    feat: Add style customize of slider

--- stories/slider.stories.js
+++ stories/slider.stories.js
@@ -110,3 +110,295 @@ storiesOf('Components|Slider', module)
       </tr>
     </table>
   `)
+
+storiesOf('Components|Slider/スタイルのカスタマイズ', module)
+  .add('スライドされた時の色を変える', () => `
+    <h4>スライドされた（オン）状態の色を指定した色に変える。</h4>
+    <rs-slider class="my-slider -slide-orange"></rs-slider>
+    <h4>使用するmixin</h4>
+    ${copyCodeBlock(
+`@include rs-slider-highlight-color($color);`,
+    { lang: 'scss' }
+  )}
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-slider class="my-slider -slide-orange" />`,
+    { lang: 'html' }
+  )}
+    ${copyCodeBlock(
+`@import '@rsmdc/slider/rs-slider.scss';
+
+.my-slider {
+  &.-slide-orange {
+    @include rs-slider-highlight-color(blue);
+  }
+}
+`,
+    { lang: 'scss' }
+  )}
+    <p>オプション</p>
+    <ul>
+      <li>
+        $color: インクの色コード <br> 例）#ee00ce
+      </li>
+    </ul>
+  `)
+  
+  .add('レールの色を変える', () => `
+    <h4>レールの色を指定した色に変える。</h4>
+    <rs-slider class="my-slider -rail-orange"></rs-slider>
+    <h4>使用するmixin</h4>
+    ${copyCodeBlock(
+`@include rs-slider-rail-color($color);`,
+    { lang: 'scss' }
+  )}
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-slider class="my-slider -rail-orange" />`,
+    { lang: 'html' }
+  )}
+    ${copyCodeBlock(
+`@import '@rsmdc/slider/rs-slider.scss';
+
+.my-slider {
+  &.-rail-orange {
+    @include rs-slider-rail-color(orange);
+  }
+}
+`,
+    { lang: 'scss' }
+  )}
+    <p>オプション</p>
+    <ul>
+      <li>
+        $color: インクの色コード <br> 例）#ee00ce
+      </li>
+    </ul>
+  `)
+
+
+  .add('マーカーの色を変える', () => `
+    <h4>マーカー（目盛り）の色を指定した色に変える。</h4>
+    <p>marked属性をつけた場合のみ有効。</p>
+    <rs-slider class="my-slider -mark-orange" marked countable></rs-slider>
+    <h4>使用するmixin</h4>
+    ${copyCodeBlock(
+`@include rs-slider-rail-tick-mark-color($color);`,
+    { lang: 'scss' }
+  )}
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-slider class="my-slider -mark-orange" marked countable />`,
+    { lang: 'html' }
+  )}
+    ${copyCodeBlock(
+`@import '@rsmdc/slider/rs-slider.scss';
+
+.my-slider {
+  &.-mark-orange {
+    @include rs-slider-rail-tick-mark-color(orange);
+  }
+}
+`,
+    { lang: 'scss' }
+  )}
+    <p>オプション</p>
+    <ul>
+      <li>
+        $color: インクの色コード <br> 例）#ee00ce
+      </li>
+    </ul>
+  `)
+
+  .add('つまみの色を変える', () => `
+    <h4>つまみ部分の色を指定した色に変える。</h4>
+    <rs-slider class="my-slider -thumb-orange"></rs-slider>
+    <h4>使用するmixin</h4>
+    ${copyCodeBlock(
+`@include rs-slider-thumb-color($color);`,
+    { lang: 'scss' }
+  )}
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-slider class="my-slider -thumb-orange" />`,
+    { lang: 'html' }
+  )}
+    ${copyCodeBlock(
+`@import '@rsmdc/slider/rs-slider.scss';
+
+.my-slider {
+  &.-thumb-orange {
+    @include rs-slider-thumb-color(orange);
+  }
+}
+`,
+    { lang: 'scss' }
+  )}
+    <p>オプション</p>
+    <ul>
+      <li>
+        $color: インクの色コード <br> 例）#ee00ce
+      </li>
+    </ul>
+  `)
+
+  .add('つまみの枠線の色を変える', () => `
+    <h4>つまみの枠線の色を指定した色に変える。</h4>
+    <rs-slider class="my-slider -stroke-orange"></rs-slider>
+    <h4>使用するmixin</h4>
+    ${copyCodeBlock(
+`@include rs-slider-thumb-stroke-cutout($color);`,
+    { lang: 'scss' }
+  )}
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-slider class="my-slider -stroke-orange" />`,
+    { lang: 'html' }
+  )}
+    ${copyCodeBlock(
+`@import '@rsmdc/slider/rs-slider.scss';
+
+.my-slider {
+  &.-stroke-orange {
+    @include rs-slider-thumb-stroke-cutout(orange);
+  }
+}
+`,
+    { lang: 'scss' }
+  )}
+    <p>オプション</p>
+    <ul>
+      <li>
+        $color: インクの色コード <br> 例）#ee00ce
+      </li>
+    </ul>
+  `)
+  
+  .add('フォーカス時の色を変える', () => `
+    <h4>フォーカス時の色を指定した色に変える。</h4>
+    <rs-slider class="my-slider -focus-orange"></rs-slider>
+    <h4>使用するmixin</h4>
+    ${copyCodeBlock(
+`@include rs-slider-focus-halo-color($color);`,
+    { lang: 'scss' }
+  )}
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-slider class="my-slider -focus-orange" />`,
+    { lang: 'html' }
+  )}
+    ${copyCodeBlock(
+`@import '@rsmdc/slider/rs-slider.scss';
+
+.my-slider {
+  &.-focus-orange {
+    @include rs-slider-focus-halo-color(orange);
+  }
+}
+`,
+    { lang: 'scss' }
+  )}
+    <p>オプション</p>
+    <ul>
+      <li>
+        $color: インクの色コード <br> 例）#ee00ce
+      </li>
+    </ul>
+  `)
+
+  .add('ピンの背景色を変える', () => `
+    <h4>ピンの背景色を指定した色に変える。</h4>
+    <p>countable属性をつけた場合のみ有効。</p>
+    <rs-slider class="my-slider -pin-fill-orange" countable></rs-slider>
+    <h4>使用するmixin</h4>
+    ${copyCodeBlock(
+`@include rs-slider-value-pin-fill-color($color);`,
+    { lang: 'scss' }
+  )}
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-slider class="my-slider -pin-fill-orange" countable />`,
+    { lang: 'html' }
+  )}
+    ${copyCodeBlock(
+`@import '@rsmdc/slider/rs-slider.scss';
+
+.my-slider {
+  &.-pin-fill-orange {
+    @include rs-slider-value-pin-fill-color(orange);
+  }
+}
+`,
+    { lang: 'scss' }
+  )}
+    <p>オプション</p>
+    <ul>
+      <li>
+        $color: インクの色コード <br> 例）#ee00ce
+      </li>
+    </ul>
+  `)
+
+  .add('ピンのテキストの色を変える', () => `
+    <h4>ピンのテキストの色を指定した色に変える。</h4>
+    <rs-slider class="my-slider -pin-orange" countable></rs-slider>
+    <h4>使用するmixin</h4>
+    ${copyCodeBlock(
+`@include rs-slider-value-pin-ink-color($color);`,
+    { lang: 'scss' }
+  )}
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-slider class="my-slider -pin-orange" countable />`,
+    { lang: 'html' }
+  )}
+    ${copyCodeBlock(
+`@import '@rsmdc/slider/rs-slider.scss';
+
+.my-slider {
+  &.-pin-orange {
+    @include rs-slider-value-pin-ink-color(orange);
+  }
+}
+`,
+    { lang: 'scss' }
+  )}
+    <p>オプション</p>
+    <ul>
+      <li>
+        $color: インクの色コード <br> 例）#ee00ce
+      </li>
+    </ul>
+  `)
+
+  .add('スライダーの色を一括で変える', () => `
+    <h4>レール、マーカー、つまみ、フォーカス、ピンの色を一括で指定した色に変える。</h4>
+    <rs-slider class="my-slider -orange"></rs-slider>
+    <h4>使用するmixin</h4>
+    ${copyCodeBlock(
+`@include rs-slider-color-accessible($color);`,
+    { lang: 'scss' }
+  )}
+    <h4>使用方法</h4>
+    ${copyCodeBlock(
+`<rs-slider class="my-slider -orange" />`,
+    { lang: 'html' }
+  )}
+    ${copyCodeBlock(
+`@import '@rsmdc/slider/rs-slider.scss';
+
+.my-slider {
+  &.-orange {
+    @include rs-slider-color-accessible(orange);
+  }
+}
+`,
+    { lang: 'scss' }
+  )}
+    <p>オプション</p>
+    <ul>
+      <li>
+        $color: インクの色コード <br> 例）#ee00ce
+      </li>
+    </ul>
+  `)
